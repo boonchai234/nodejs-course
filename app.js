750f811f524f940d34a5db102b2d91524b8d597d
boonchai234 <boonlek2011@gmail.com>
2024-02-28 16:02:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const debug = require ('debug')('app');
+const debug = require('debug')('app');
 const morgan = require('morgan')
-const path = require ('path');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const productrouter = express.Router();
 
 app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname,"/public/business-template")));
-app.set("views","./src/views");
-app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname, "/public/business-template")));
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
 
-app.get("/",(req,res) =>{
+productrouter.route("/").get((req, res) => {
+    res.send("Hello Product")
+});
+productrouter.route("/1").get((req, res) => {
+    res.send("Hello Product_1")
+});
+app.use("/product", productrouter)
 
-    res.render('index',{username: "5555",customer:["kitti","kitty","Kittja"]});
+app.get("/", (req, res) => {
+
+    res.render('index', { username: "5555", customer: ["kitti", "kitty", "Kittja"] });
 })
-app.listen(PORT,()=>{
-    console.log("Listening on port",PORT);
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT);
 })
