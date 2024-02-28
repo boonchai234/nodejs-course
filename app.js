e16412bbd4a38aa6f9792687c5a209b92f079fc8
boonchai234 <boonlek2011@gmail.com>
2024-02-28 15:31:40 +0700

    summary

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const chalk = require('chalk');
 const debug = require ('debug')('app');
 const morgan = require('morgan')
+const path = require ('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/business-template")));
+app.set("views","./src/views");
+app.set("view engine","ejs")
+
 app.get("/",(req,res) =>{
 
-    res.send('Hello world');
+    res.render('index',{username: "5555",customer:["kitti","kitty","Kittja"]});
 })
-app.listen(port,()=>{
-    console.log("Listening on port"+ chalk.green(port));
+app.listen(PORT,()=>{
+    console.log("Listening on port",PORT);
 })
